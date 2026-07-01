61a6afe66d474630c1902c519b8651755be903c6
sohel <sohelarfi0@gmail.com>
2026-07-01 20:42:33 +0600

    New Chatting site

---
+++ client/src/app/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function _layout() {
+  return (
+    <View>
+      <Text>
+        Tabs_layout</Text>
+    </View>
+  )
+}
