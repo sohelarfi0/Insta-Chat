4082969ed7f58aee45e5dd667ea41cf45b3c45cf
sohel <sohelarfi0@gmail.com>
2026-07-03 22:11:35 +0600

    complete the sign in and sign up page

--- client/src/app/(auth)/index.tsx
+++ client/src/app/(auth)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Module, KeyboardAvoidingView, Platform, ScrollView } from 'react-native'
+import {TouchableOpacity, View, Text, Module, KeyboardAvoidingView, Platform, ScrollView, ActivityIndicator } from 'react-native'
 import React, {useState} from 'react'
 import { useRouter } from 'expo-router'
 import {SafeAreaView} from 'react-native-safe-area-context'
@@ -6,6 +6,8 @@ import {styles} from '@/assets//styles/AuthScreen.styles'
 import {LinearGradient} from 'expo-linear-gradient'
 import { Colors } from '../../../constants/Colors'
 import {SvgXml} from 'react-native-svg'
+import { TextInput } from 'react-native-gesture-handler'
+import {Ionicons} from '@expo/vector-icons'
 
 type Mode = "login" | "register"
 
@@ -15,12 +17,27 @@ export default function AuthScreen() {
   const [handle, setHandle] = useState(" ")
   const [email, setEmail] = useState(" ")
   const [password, setPassword] = useState(" ")
-  const [verification, setVerification] = useState(" ")
+  const [verificationCode, setVerificationCode] = useState(" ")
   const [loading, setLoading] = useState(false)
   const [verifying, setVerifying] = useState(false)
 
   const router = useRouter();
 
+  const handleSubmit = async () => {
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+      setVerifying(true)
+    },1500)
+  }
+  
+  const handleVerify = async () => {
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+      router.replace("/(tabs)")
+    },1500)
+  }
   const svgMarkup= `<svg width="63" height="70" viewBox="0 0 63 70" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M33.817 52.382c0-15.988 12.96-28.948 28.948-28.948v17.585c0 15.987-12.96 28.948-28.948 28.948zm-4.869 0c0-15.988-12.96-28.948-28.948-28.948v17.585c0 15.987 12.96 28.948 28.948 28.948z" fill="#fff"/>
   <g clip-path="url(#a)">
@@ -32,6 +49,118 @@ export default function AuthScreen() {
     </clipPath>
   </defs>
 </svg>`
+
+  if(verifying){
+    return(
+      <SafeAreaView style={styles.safe}>
+      <KeyboardAvoidingView style={styles.kav} 
+      behavior={Platform.OS === "ios" ? "padding" : undefined}>
+        <ScrollView contentContainerStyle={styles.scroll}
+        keyboardShouldPersistTaps="handled">
+          {/*  Logo */}
+          <View style={styles.logoRow}>
+            <LinearGradient colors={[Colors.primary, Colors.primaryContainer]} style={styles.logoBox}>
+             
+             <SvgXml xml={svgMarkup}  width="50%" height="50%"/>
+              </LinearGradient>
+              <Text style={styles.appName}>InstaChat</Text>
+
+          </View>
+          {/* Hero text */}
+          <Text style={styles.heading}>
+            Verify Email
+          </Text>
+          <Text style={styles.subheading}>
+            We have sent a 6-digit verification code to {email}.
+          </Text>
+          {/* Form */}
+
+          <View style={styles.form}>
+            
+
+            <View style={styles.field}>
+              <Text style={styles.fieldLabel}>
+                Verification Code
+
+              </Text>
+              <TextInput
+                style={styles.input}
+                value={verificationCode}
+                onChangeText={setVerificationCode}
+                placeholder='Enter 6-digit code'
+                placeholderTextColor={Colors.outlineVariant}
+                autoCapitalize="none"
+                keyboardType='number-pad'
+              />
+
+            </View>
+
+            <View style={styles.field}>
+              <Text style={styles.fieldLabel}>
+                Password
+
+              </Text>
+              <TextInput
+                style={styles.input}
+                value={password}
+                onChangeText={setPassword}
+                placeholder='*******'
+                placeholderTextColor={Colors.outlineVariant}
+                secureTextEntry
+              />
+
+            </View>
+            {/* Back to sign up link  */}
+
+            <View style={styles.toggleRow}>
+              <Text style={styles.toggleText}>Did not receive a code ?
+
+              </Text>
+              <TouchableOpacity onPress={()=>setVerifying(false)}>
+                <Text style={styles.toggleLink}>Go back</Text>
+              </TouchableOpacity>
+            </View>
+
+            {/* Submit */}
+            <TouchableOpacity onPress={handleVerify}
+             disabled={loading} activeOpacity={0.88}
+            style={styles.btnWrapper}>
+              <LinearGradient colors={[Colors.primary, Colors.primaryContainer]}
+              start={{x:0,y:0}}
+              end={{x:1, y:1}}
+              style={styles.btn}>
+              
+                {loading? (
+                    <ActivityIndicator color={Colors.onPrimary}
+                    size="small"/>
+
+                  ):
+                  (
+                    <>
+                    <Text style={styles.btnText}>
+                      Verify code
+                    </Text>
+                    <Ionicons name='arrow-forward' 
+                    size={18} color={Colors.onPrimary}/>
+                    </>
+
+                  )
+}
+              </LinearGradient>
+            </TouchableOpacity>
+
+
+          </View>
+
+          
+          </ScrollView>
+
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+    )
+  }
+
+
   return (
     <SafeAreaView style={styles.safe}>
       <KeyboardAvoidingView style={styles.kav} 
@@ -60,12 +189,113 @@ export default function AuthScreen() {
             {mode === "register" && (
               <>
               <View style={styles.field}>
-                
+                <Text style={styles.fieldLabel} >Full Name</Text>
+                <TextInput
+                style={styles.input}
+                value={name}
+                onChangeText={setName}
+                placeholder= 'Your name'
+                placeholderTextColor={Colors.outlineVariant}
+                autoCapitalize='words' />
+
+              </View>
+              <View style={styles.field}>
+                <Text style={styles.fieldLabel}>Username Handle
+                </Text>
+                <View style={styles.handleRow} >
+                  <Text style={styles.atSign}>@</Text>
+                  <TextInput
+                   style={[styles.input, styles.handleInput]}
+                   value={handle}
+                   onChangeText={(v)=>setHandle(v.toLocaleLowerCase().replace(/\s/g,""))}
+                   placeholder='username'
+                   autoCapitalize='none'
+                  />
+
+                  </View>
 
               </View>
               
               </>
             )}
+
+            <View style={styles.field}>
+              <Text style={styles.fieldLabel}>
+                Email
+
+              </Text>
+              <TextInput
+                style={styles.input}
+                value={email}
+                onChangeText={setEmail}
+                placeholder='Your email'
+                placeholderTextColor={Colors.outlineVariant}
+                autoCapitalize='none'
+                keyboardType='email-address'
+              />
+
+            </View>
+
+            <View style={styles.field}>
+              <Text style={styles.fieldLabel}>
+                Password
+
+              </Text>
+              <TextInput
+                style={styles.input}
+                value={password}
+                onChangeText={setPassword}
+                placeholder='*******'
+                placeholderTextColor={Colors.outlineVariant}
+                secureTextEntry
+              />
+
+            </View>
+            {/* Toggle mode */}
+            <View style={styles.toggleRow}>
+              <Text style={styles.toggleText}>
+                {mode=== "login" ? "Don't have an account?" :
+                "Already have an account?"}
+              </Text>
+              <TouchableOpacity onPress={()=> setMode(mode ===
+                "login" ? "register" : "login"
+              )}>
+                
+                <Text style={styles.toggleLink}>
+                  {mode === "login" ? "sign up" : "Sign in"}
+
+                </Text>
+              </TouchableOpacity>
+
+            </View>
+            {/* Submit */}
+            <TouchableOpacity onPress={handleSubmit}
+             disabled={loading} activeOpacity={0.88}
+            style={styles.btnWrapper}>
+              <LinearGradient colors={[Colors.primary, Colors.primaryContainer]}
+              start={{x:0,y:0}}
+              end={{x:1, y:1}}
+              style={styles.btn}>
+              
+                {loading? (
+                    <ActivityIndicator color={Colors.onPrimary}
+                    size="small"/>
+
+                  ):
+                  (
+                    <>
+                    <Text style={styles.btnText}>
+                      {mode=== "login" ? "Sign In" : "Create Account"}
+                    </Text>
+                    <Ionicons name='arrow-forward' 
+                    size={18} color={Colors.onPrimary}/>
+                    </>
+
+                  )
+}
+              </LinearGradient>
+            </TouchableOpacity>
+
 
           </View>
 
